b58b8df46e8ce6fed1cf36ca5fb92c5fb8491577
minjun06 <35772631+minjun06@users.noreply.github.com>
2021-06-18 23:44:35 +0900

    added NY and Paris

--- components/travel/Travel.js
+++ components/travel/Travel.js
@@ -17,18 +17,18 @@ export default function Travel({route,navigation}) {
           image={'Tokyo-2021.jpg'}
       />
       <ListItem
-          title={'My Summer Vacation'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt'}
-          date={'18 June 2020'}
-          location={'Tokyo, Japan'}
-          image={'Tokyo-2021.jpg'}
+          title={'My Favorite Place'}
+          content={'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+          date={'5 December 2018'}
+          location={'New York, USA'}
+          image={'NewYork.jpg'}
       />
       <ListItem
-          title={'My Summer Vacation'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt'}
-          date={'18 June 2020'}
-          location={'Tokyo, Japan'}
-          image={'Tokyo-2021.jpg'}
+          title={'My Dream City'}
+          content={'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'}
+          date={'00 AAA 2050'}
+          location={'Paris, France'}
+          image={'paris.jpg'}
       />
     </View>
   );
