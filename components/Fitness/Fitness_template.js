372474076fbf1c8cfb081c2334cf3ce57cff899a
YiZheHong <yhong@brandeis.edu>
2021-06-20 21:50:00 -0400

    Make a goal button in Fit. temp.

--- components/Fitness/Fitness_template.js
+++ components/Fitness/Fitness_template.js
@@ -1,50 +1,92 @@
-/*
+import React, { useState } from "react";
+import { Alert,
+  Modal,
+  StyleSheet,
+   Text,
+   Pressable,
+   View ,
+   Button,
+     SafeAreaView,
+     TouchableOpacity,} from "react-native";
 
-*/
-import React from 'react';
-import { StyleSheet, Image, Text, View } from 'react-native';
-
-export default function Fitness({route,navigation}) {
+const Fitness_Template = (props) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [Goal, setGoal] = useState(props.Goal);
   return (
-    <View>
-      <ListItem
-          title={'My Summer Vacation'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt'}
-          date={'18 June 2020'}
-          location={'Tokyo, Japan'}
-          image={'Tokyo-2021.jpg'}
-      />
-      <ListItem
-          title={'My Favorite Place'}
-          content={'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
-          date={'5 December 2018'}
-          location={'New York, USA'}
-          image={'NewYork.jpg'}
-      />
-      <ListItem
-          title={'My Dream City'}
-          content={'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'}
-          date={'00 AAA 2050'}
-          location={'Paris, France'}
-          image={'paris.jpg'}
-      />
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>{Goal}</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </Pressable>
     </View>
   );
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection:'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin:'25%',
-    marginTop:20,
-    padding:20,
-  },
-  headerText: {
-    fontSize: 40,
-    color: 'blue'
-  },
+};
 
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 });
+
+export default App;
