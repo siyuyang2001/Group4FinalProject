1d35ec30a827b8024a6c8d70eca6af110b6cede8
YiZheHong <yhong@brandeis.edu>
2021-06-20 20:16:06 -0400

    Make some edit on fitness page

--- components/Fitness/Fitness.js
+++ components/Fitness/Fitness.js
@@ -2,7 +2,6 @@
 
 */
 import React from 'react';
-// import { EditOutlined } from '@ant-design/icons';
 import { Button,
   SafeAreaView,
   StyleSheet,
@@ -12,7 +11,9 @@ import { Button,
   TouchableOpacity,} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ReactDOM from 'react-dom';
+
 
 import Abs from './Abs'
 import Back from './Back'
@@ -22,9 +23,13 @@ import Record from './Record'
 import SmallM from './SmallM'
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
   const MyFitnessScreen = () => {
    return (
      <Tab.Navigator>
+     <Stack.Screen name="Fitness" component={FitnessPage}
+           options={{ title: 'Fitness' }}/>
         <Stack.Screen name="Abs" component={Abs}
            options={{ title: 'Abs' }}/>
         <Stack.Screen name="Back" component={Back}
@@ -33,75 +38,148 @@ const Stack = createStackNavigator();
           options={{title: 'Chest'}}/>
         <Stack.Screen name="Legs" component={Legs}
            options={{title: 'Legs'}}/>
+           <Stack.Screen name="Small_Muscle" component={SmallM}
+               options={{title: 'Small_Muscle'}}/>
            <Stack.Screen name="Record" component={Record}
               options={{title: 'Record'}}/>
-            <Stack.Screen name="SmallM" component={SmallM}
-               options={{title: 'SmallM'}}/>
+
 </Tab.Navigator>
 
    );
  };
 
  // Here is a homescreen with some navigation ...
- const HomeScreen = ({ navigation }) => {
+ const FitnessPage = ({ navigation }) => {
    return (
      <View style={styles.container}>
-       <Button
-         title="Work On Chest"
-         color="#e9967a"
-         onPress={() =>
-           navigation.navigate('Chest')
-         }
-       />
+     <View style={styles.buttonIconSeparatorStyle}>
+       <TouchableOpacity
+          style={styles.buttonChestStyle}
+          activeOpacity={0.5}>
+          <Image
+            source={{
+              uri:
+                'https://cdn1.iconfinder.com/data/icons/miscellaneous-156-line/128/chest_bosom_breastplate_body_man_human_anatomy_fit_health-512.png',
+            }}
+            style={styles.buttonImageIconStyle}
+          />
+          <View style={styles.buttonIconSeparatorStyle} />
+           <Button
+       title="Work On Chest"
+       color='#87cefa'
+       onPress={() =>
+         navigation.navigate('Chest')
+       }
+     />
+      </TouchableOpacity>
        <Text>  {"\n"} </Text>
-       <Button
-         title="Work On Back"
-         color="#b0c4de"
-         onPress={() =>
-           navigation.navigate('Back')
-         }
-       />
+       <TouchableOpacity
+          style={styles.buttonBackStyle}
+          activeOpacity={0.5}>
+          <Image
+            source={{
+              uri:
+                'https://cdn4.iconfinder.com/data/icons/fitness-vol-2-glyph/512/Back_strong_muscle_exercise_body_parts_medical_sports_man_fitness_natomy-512.png',
+            }}
+            style={styles.buttonImageIconStyle}
+          />
+          <View style={styles.buttonIconSeparatorStyle} />
+           <Button
+       title="Work On Back"
+       color='#f0e68c'
+       onPress={() =>
+         navigation.navigate('Back')
+       }
+     />
+      </TouchableOpacity>
          <Text>  {"\n"} </Text>
-       <Button
-         title="Work On Abs"
-         color="#add8e6"
-         onPress={() =>
-           navigation.navigate('Abs')
-         }
-       />
+       <TouchableOpacity
+          style={styles.buttonAbsStyle}
+          activeOpacity={0.5}>
+          <Image
+            source={{
+              uri:
+                'https://cdn0.iconfinder.com/data/icons/fitness-58/50/Fitness-06-512.png',
+            }}
+            style={styles.buttonImageIconStyle}
+          />
+          <View style={styles.buttonIconSeparatorStyle} />
+           <Button
+       title="Work On Abs"
+       color='#dda0dd'
+       onPress={() =>
+         navigation.navigate('Abs')
+       }
+     />
+      </TouchableOpacity>
+
          <Text>  {"\n"} </Text>
-       <Button
-         title="Work On Legs"
-         color="#8fbc8f"
-         onPress={() =>
-           navigation.navigate('Legs')
-         }
-       />
+
+          <TouchableOpacity
+          style={styles.buttonLegsStyle}
+          activeOpacity={0.5}>
+          <Image
+            source={{
+              uri:
+                'https://cdn2.iconfinder.com/data/icons/fitness-outline-4/512/body_part_anatomy_leg_muscle_foot-512.png',
+            }}
+            style={styles.buttonImageIconStyle}
+          />
+          <View style={styles.buttonIconSeparatorStyle} />
+           <Button
+       title="Work on Legs"
+       color='#ffa07a'
+       onPress={() =>
+         navigation.navigate('Legs')
+       }
+     />
+        </TouchableOpacity>
          <Text>  {"\n"} </Text>
-       <Button
-         title="Work On Small Muscle"
-         color="#add8e6"
-         onPress={() =>
-           navigation.navigate('SmallM')
-         }
-       />
+
+        <TouchableOpacity
+          style={styles.buttonSmallMStyle}
+          activeOpacity={0.5}>
+          <Image
+            source={{
+              uri:
+                'https://cdn.iconscout.com/icon/premium/png-256-thumb/strong-arm-1947587-1654772.png',
+            }}
+            style={styles.buttonImageIconStyle}
+          />
+          <View style={styles.buttonIconSeparatorStyle} />
+           <Button
+       title="Small Muscles"
+       color='#ffe4c4'
+       onPress={() =>
+         navigation.navigate('SmallM')
+       }
+     />
+      </TouchableOpacity>
          <Text>  {"\n"} </Text>
-         <View style={styles.container}>
+
          <TouchableOpacity
-        style={styles.buttonFacebookStyle}
-        activeOpacity={0.5}>
-        // <EditOutlined style = {{height: 25,  width: 35,}}/>
-        <View style={styles.buttonIconSeparatorStyle} />
-        <Button
-       title="Record"
+          style={styles.buttonFacebookStyle}
+          activeOpacity={0.5}>
+          <Image
+            source={{
+              uri:
+                'https://static.thenounproject.com/png/75540-200.png',
+            }}
+            style={styles.buttonImageIconStyle}
+          />
+          <View style={styles.buttonIconSeparatorStyle} />
+           <Button
+       title="Make A Record"
        color='#485a96'
        onPress={() =>
          navigation.navigate('Record')
        }
      />
-      </TouchableOpacity>
+
+        </TouchableOpacity>
          </View>
-     </View>
+
+     </View> //54 view
    );
  };
 
@@ -118,10 +196,50 @@ const Stack = createStackNavigator();
      fontSize: 40,
      color: 'blue'
    },
-  buttonGPlusStyle: {
+  buttonSmallMStyle: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#dc4e41',
+    backgroundColor: '#ffe4c4',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: 40,
+    borderRadius: 5,
+    margin: 5,
+  },
+    buttonAbsStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#dda0dd',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: 40,
+    borderRadius: 5,
+    margin: 5,
+  },
+      buttonLegsStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#ffa07a',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: 40,
+    borderRadius: 5,
+    margin: 5,
+  },
+       buttonChestStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#87cefa',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: 40,
+    borderRadius: 5,
+    margin: 5,
+  },
+  buttonBackStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#f0e68c',
     borderWidth: 0.5,
     borderColor: '#fff',
     height: 40,
@@ -145,15 +263,18 @@ const Stack = createStackNavigator();
     width: 25,
     resizeMode: 'stretch',
   },
-  buttonTextStyle: {
+  button: {
     color: '#fff',
     marginBottom: 4,
     marginLeft: 10,
+    width: '100%',
+      height: '30%',
+    textAlign:'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   buttonIconSeparatorStyle: {
     backgroundColor: '#fff',
-    width: 1,
-    height: 40,
   },
 
  });
