ca05fe5cd59b82b3a49322b7d5acc0fdea72aba8
cry319 <85949474+cry319@users.noreply.github.com>
2021-06-22 21:31:54 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Travel from './components/travel/Travel.js'
 import Fitness from './components/Fitness/Fitness.js'
-import Reading from './components/Reading/ReadingList.js'
+import Reading from './components/ReadingList.js'
 import Group4 from './components/Group4'
 
 
