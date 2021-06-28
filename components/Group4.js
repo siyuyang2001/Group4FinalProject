6694a8ccb176eceb02b6b18af7fa80f34b1d3b5a
siyuyang2001 <siyuyang@brandeis.edu>
2021-06-28 11:20:20 -0400

    intro change

--- components/Group4.js
+++ components/Group4.js
@@ -1,80 +1,106 @@
-
 import React from 'react';
 import { StyleSheet, Image, Text, View,SafeAreaView} from 'react-native';
-import { Button, Icon } from 'react-native-elements'
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+
 
 
 export default function Group4Profile({route,navigation}) {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.headerText}> Group4's Profile </Text>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.headerText}> Group4's Profile </Text>
       <Text style={styles.body}>  {"\n"}Welcome to Group4!
       </Text>
-
-      <Text>  {"\n"} </Text>
-      <div>
-    <Text style={{fontWeight: "bold"}}>   Siyu Yang：</Text>
-      Cosi and econ major. Love cooking and anything related to fashion!
-    <Text>  {"\n"} </Text>
-      </div>
-      <div>
-    <Text style={{fontWeight: "bold"}}>  YiZhe Hong: </Text>
-     I am a junior majoring in Computer Science and Applied Mathematics. I will be a good programmer in the future!
-    <Text>  {"\n"} </Text>
-      </div>
-
-      <div>
-    <Text style={{fontWeight: "bold"}}>   Kingsley Zhang: </Text>
-      Rising senior majoring in CS, Econ and Applied Mathematics. I have a cat named Java.
-    <Text>  {"\n"} </Text>
-      </div>
-      <div>
-    <Text style={{fontWeight: "bold"}}>   MinJun Song: </Text>
-      Cosi and econ major, rising senior from Korea.
-    <Text>  {"\n"} </Text>
-      </div>
-      <div>
-     <Text style={{fontWeight: "bold"}}> Ruyi Cai: </Text>
-      I’m a junior majoring in COSI and IGS. I love reading comics and playing classical music in my free time.
-     <Text>  {"\n"} </Text>
-      </div>
-      <div>
-     <Text style={{fontWeight: "bold"}}>   Richard Hu: </Text>
-      Rising junior majoring in chemistry. Love playing soccer and reading novels.
-     <Text>  {"\n"} </Text>
-      </div>
-
       <Image
         style= {{ width: 305, height: 300}}
         source=  {require('../assets/cat.png')}
         alt = "Group pic"
       />
-      Meet our group mascot Java!
+      <Text style={styles.body}>  {"\n"}  Meet our group mascot Java!</Text>
+
+
+                <Card>
+                  <Card.Content>
+                    <Title style={{fontSize: 30, fontWeight: "bold",marginTop:10}}>Yizhe Hong</Title>
+                    <Paragraph>  I am a junior majoring in Computer Science and Applied Mathematics. {'\n'}I will be a good programmer in the future!</Paragraph>
+                    </Card.Content>
+                    <Card.Cover source={{ uri: 'https://picsum.photos/701' }} />
+                </Card>
+
+        <Card>
+          <Card.Content>
+            <Title style={{fontSize: 30, fontWeight: "bold",marginTop:10}}>Ruyi Cai</Title>
+            <Paragraph> I’m a junior majoring in COSI and IGS. {'\n'}I love reading comics and playing classical music in my free time.</Paragraph>
+            </Card.Content>
+            <Card.Cover source={{ uri: 'https://picsum.photos/701' }} />
+        </Card>
+
+
+        <Card>
+          <Card.Content>
+            <Title style={{fontSize: 30, fontWeight: "bold",marginTop:10}}>Kingsley Zhang </Title>
+            <Paragraph> Rising senior majoring in CS, Econ and applied Mathematics.{'\n'}I have a cat named Java.</Paragraph>
+            </Card.Content>
+            <Card.Cover source={{ uri: 'https://picsum.photos/701' }} />
+        </Card>
+        <Card>
+          <Card.Content>
+            <Title style={{fontSize: 30, fontWeight: "bold",marginTop:10}}>Richard Hu</Title>
+            <Paragraph>   Rising junior majoring in chemistry.{'\n'} Love playing soccer and reading novels.</Paragraph>
+            </Card.Content>
+            <Card.Cover source={{ uri: 'https://picsum.photos/701' }} />
+        </Card>
+
+        <View style={{flex: 5, flexDirection: 'row'}}>
+          <Card>
+              <Card.Content>
+                <Title style={{fontSize: 30, fontWeight: "bold",marginTop:10}}>Minjun Song</Title>
+                <Paragraph>   Cosi and econ major, rising senior from Korea.</Paragraph>
+              </Card.Content>
+              <Card.Cover source={{ uri: 'https://picsum.photos/701' }} />
+            </Card>
+
+        </View>
+          
+        <Card>
+            <Card.Content>
+              <Title style={{fontSize: 30, fontWeight: "bold",marginTop:10}}>Siyu Yang</Title>
+              <Paragraph> Rising junior with cosi and econ major.{'\n'}I Love cooking and anything related to fashion!</Paragraph>
+            </Card.Content>
+            <Card.Cover source={{ uri: 'https://picsum.photos/701' }} />
+        </Card>
+
 
       <Button title="Go Home"
           onPress={() => navigation.navigate('Home')} />
 
-    </SafeAreaView >
+
+      </View>
+
+    </SafeAreaView>
   );
-}
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection:'column',
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-
+    paddingTop: 40,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
   },
   headerText: {
     fontSize: 35,
   },
   body: {
     fontSize: 25,
-  },
-  middle:{
-    alignItems: 'center',
-      justifyContent: 'center',
   },
 
 });
